230669f63c3a2afaeb4e68814a9cd97099df09ab
hare-care <harisbajramovic2024@u.northwestern.edu>
2024-01-09 23:00:50 -0600

    added ifetch to top module

--- src/pipeline_top.v
+++ src/pipeline_top.v
@@ -4,8 +4,8 @@ module pipeline_top (
 );
 
 // Pipeline register nets
-wire [31:0] PC4_IF;
-reg [31:0] PC4_Dec, PC4_Exec, PC4_Mem;
+wire [31:0] PC_IF;
+reg [31:0] PC_Dec, PC_Exec, PC_Mem;
 wire [31:0] Instruction_IF;
 reg [31:0] Instruction_Dec;
 reg [31:0] Imm_Exec, Imm_Dec;
@@ -18,11 +18,11 @@ reg [31:0] ALU_output_Exec, ALU_output_Mem, ALU_output_WB;
 reg [31:0] Zero_Exec, Zero_Mem;
 reg [31:0] Data_mem_Mem, Data_mem_WB;
 
-assign PC4_IF = test;
-
+wire [31:0] branch_pc;
+wire npc_control;
 
 // fetch
-//ifetch fetch (.branch_pc(), .npc_control, .instruction(), .PC4 ());
+ifetch fetch (.branch_pc(), .npc_control(), .instruction(Instruction_IF), .PC (PC_IF));
 
 // decode
 //decode_stage decode ((), );
@@ -38,17 +38,17 @@ assign PC4_IF = test;
 // pipeline registers
 always @(posedge clk or negedge rstn) begin 
     if (!rstn) begin
-        PC4_Dec <= 32'b0;
+        PC_Dec <= 32'b0;
         Instruction_Dec <= 32'b0;
 
-        PC4_Exec <= 32'b0;
+        PC_Exec <= 32'b0;
         A_Exec <= 32'b0;
         B_Exec <= 32'b0;
         Imm_Exec <= 32'b0;
         Rt_Exec <= 32'b0;
         Rd_Exec <= 32'b0;
 
-        PC4_Mem <= 32'b0;
+        PC_Mem <= 32'b0;
         Zero_Mem <= 32'b0;
         ALU_output_Mem <= 32'b0;
         B_Mem <= 32'b0;
@@ -59,17 +59,17 @@ always @(posedge clk or negedge rstn) begin
         ALU_output_WB <= 32'b0;
     end
     else begin
-        PC4_Dec <= PC4_IF;
+        PC_Dec <= PC_IF;
         Instruction_Dec <= Instruction_IF;
 
-        PC4_Exec <= PC4_Dec;
+        PC_Exec <= PC_Dec;
         A_Exec <= A_Dec;
         B_Exec <= B_Dec;
         Imm_Exec <= Imm_Dec;
         Rt_Exec <= Rt_Dec;
         Rd_Exec <= Rd_Dec;
 
-        PC4_Mem <= PC4_Exec;
+        PC_Mem <= PC_Exec;
         Zero_Mem <= Zero_Exec;
         ALU_output_Mem <= ALU_output_Exec;
         B_Mem <= B_Exec;
