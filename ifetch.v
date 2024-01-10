1e9e4423909c4325359113d60a940b35913cc10c
SiLin <145307129+SilinYe2001@users.noreply.github.com>
2024-01-09 22:55:46 -0600

    Add files via upload

---
+++ ifetch.v
@@ -0,0 +1,14 @@
+module ifetch(
+    input [31:0] branch_pc,
+    input npc_control,
+    output [31:0] instruction,
+    output [31:0]PC
+);
+    wire [31:0]NPC;
+    wire [31:0]InstWord;
+    assign NPC=npc_control ?  branch_pc: PC+4;
+   // PC register, insturction fetch
+   Reg PC_REG(.Din(NPC), .Qout(PC), .WEN(1'b0), .CLK(clk), .RST(rst));
+   InstMem IMEM(.Addr(NPC), .Size(`SIZE_WORD), .DataOut(InstWord), .CLK(clk));
+    assign instruction=InstWord;
+endmodule
