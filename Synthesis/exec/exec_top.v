61351b17dc6e725d9776dfa6231798dce4c0035d
hare-care <harisbajramovic2024@u.northwestern.edu>
2024-02-06 10:46:15 -0600

    re ran exec syntheisis

--- Synthesis/exec/exec_top.v
+++ Synthesis/exec/exec_top.v
@@ -8,17 +8,21 @@ module exec_top(
     input [31:0] imm32,
     input [31:0] PC,//PC_Exec_in
     output [31:0] ALU_result,
-    output reg jump_flag,
-    output reg [31:0]jump_target_PC 
+    output jump_flag,
+    output [31:0]jump_target_PC 
 );
 
 reg [5:0] ALU_Control_flop;
-reg [31:0] operand_A_flop, operand_B_flop, Rdata1_flop, imm32_flop, PC_flop, ALU_result_flop;
-reg branch_op_flop;
+reg [31:0] operand_A_flop, operand_B_flop, Rdata1_flop, 
+           imm32_flop, PC_flop, ALU_result_flop,
+           jump_target_PC_flop;
+reg branch_op_flop, jump_flag_flop;
 wire [31:0] ALU_result_unit, jump_target_PC_unit;
 wire jump_flag_unit;
 
 assign ALU_result = ALU_result_flop;
+assign jump_flag = jump_flag_flop;
+assign jump_target_PC = jump_target_PC_flop;
 
 
 
@@ -43,9 +47,9 @@ always @(posedge clk or negedge rstn) begin
         imm32_flop <= 32'b0;
         PC_flop <= 32'b0;
         branch_op_flop <= 1'b0;
-        jump_target_PC <= 32'b0;
+        jump_target_PC_flop <= 32'b0;
         ALU_result_flop <= 32'b0;
-        jump_flag <= 1'b0;
+        jump_flag_flop <= 1'b0;
     end
     else begin
         operand_A_flop <= operand_A;
@@ -54,9 +58,9 @@ always @(posedge clk or negedge rstn) begin
         imm32_flop <= imm32;
         PC_flop <= PC;
         branch_op_flop <= branch_op;
-        jump_target_PC <= jump_target_PC_unit;
+        jump_target_PC_flop <= jump_target_PC_unit;
         ALU_result_flop <= ALU_result_unit;
-        jump_flag <= jump_flag_unit;
+        jump_flag_flop <= jump_flag_unit;
     end
 
 end
