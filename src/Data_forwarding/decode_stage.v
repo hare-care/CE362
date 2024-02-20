3c5a59ed121685741f69c60f8e0978d4d2b3a830
Silin Ye <sfs6562@freeze.ece.northwestern.edu>
2024-02-19 21:47:34 -0600

    update decode stage

--- src/Data_forwarding/decode_stage.v
+++ src/Data_forwarding/decode_stage.v
@@ -43,6 +43,7 @@ reg wait_signal;
 reg [1:0] forward_select_A,forward_select_A_c;
 reg [1:0] forward_select_B,forward_select_B_c;
 reg jump_flag,branch_jump_flag;
+reg [1:0]predict_result;
 
 decoder decoder_unit (
       // input            
@@ -74,34 +75,9 @@ branch_unit branch_unit(
   .operand_B(operand_B),
   .branch_result(branch_result)
 );
-reg [1:0]predict_result;
-
-always @(*) begin
-    // check prediction result
-  if (imm32[31]==1'b1) begin  // it is a jump back, predict PC jump
-    if (branch_result==1'b1) begin  //right prediction,  the PC has jumped, continue
-      predict_result=2'b11; 
-    end
-    else if (branch_result==1'b0) begin //wrong prediction, the PC doesnt jump, flush 1 stage and reload the PC 
-      predict_result=1'b01;
-    end
-  end
-  else if (imm32[31]=1'b0) begin  // no jump back, predict PC+4
-    if (branch_result==1'b1) begin  //  predict wrong, PC should jump, load PC + imm
-      predict_result=1'b10;
-    end
-    else if (branch_result==1'b0) begin //  predict right, PC not jump, continue 
-        predict_result=2'b11; 
-    end
-  end
-  else begin
-    predict_result=2'b0;
-  end
-end
-
 //control PC+4 write  to register file for JAL and JALR   
 always @(*) begin
-  // save the PC when it is a jump 
+  // save the current PC+4 when it is a jump 
   if (ALU_Control==`JAL||ALU_Control==`JALR) begin
     Rdst_in=Rd_Dec;
     RWrdata=PC+4;
@@ -126,6 +102,7 @@ always @(posedge clk or negedge rstn) begin
     forward_select_B=forward_select_B_c;
   end
 end
+// comb logic
 always @(*) begin
     //default values
     forward_select_A_c=2'b00;
@@ -145,78 +122,119 @@ always @(*) begin
    //not a stall and wait, check conflict regiter or direct calculate branch and JALR
     if (wait_signal==1'b0) begin 
       // conflict with Rsrc1, wait for 1 cycle
-      if (Rd_Mem!=5'b0 && Rd_Mem==Rsrc1 && mem_wEn_Mem==1'b1) begin //conflict with Mem stage
+      if (Rd_WB!=5'b0 && Rd_WB==Rsrc1 && wrEN_WB==1'b1) begin
         wait_signal_c=1'b1;
-        forward_select_A_c=2'b01;
-        operand_A=32'b0;   
+        forward_select_A_c=2'b11;
+      end
+      else if (Rd_Mem!=5'b0 && Rd_Mem==Rsrc1 && mem_wEn_Mem==1'b1) begin //conflict with Mem stage
+        wait_signal_c=1'b1;
+        forward_select_A_c=2'b01;  
       end
       else if (Rd_Exec!= 5'b0 && Rd_Exec==Rsrc1 && mem_wEn_Exec==1'b1) begin // conflict with Exec stage
         wait_signal_c=1'b1;
-        forward_select_A_c=2'b10;
-        operand_A=32'b0;   
+        forward_select_A_c=2'b10;  
+      end
+      else begin
+      // no conflict
+        operand_A=Rdata1;
       end
       // conflict with Rsrc2, wait for 1 cycle
-      if (Rd_Mem!=5'b0 && Rd_Mem==Rsrc2 && mem_wEn_Mem==1'b1) begin
+      if (Rd_WB!=5'b0 && Rd_WB==Rsrc2 && wrEN_WB==1'b1) begin
+        wait_signal_c=1'b1;
+        forward_select_B_c=2'b11;
+      end
+      else if (Rd_Mem!=5'b0 && Rd_Mem==Rsrc2 && mem_wEn_Mem==1'b1) begin
         wait_signal_c=1'b1;
         forward_select_B_c=2'b01;   
-        operand_B=32'b0;
       end
       else if (Rd_Exec!= 5'b0 && Rd_Exec==Rsrc2 && mem_wEn_Exec==1'b1) begin
         wait_signal_c=1'b1;
         forward_select_B_c=2'b10;  
-        operand_B=32'b0;
+      end
+      else begin
+        // no conflict
+        operand_B=Rdata2;
       end
     end
     // has wait for 1 cycle, forward the data to dec stage  
     else if (wait_signal==1'b1)begin  
+      wait_signal_c=1'b0;
       //operand A forwarding
-      if(forward_select_A==2'b01) begin  //forward the data at WB stage
+      if (forward_select_A==2'b11) begin
+          operand_A=Rdata1;
+      end
+      else if(forward_select_A==2'b01) begin  //forward the data at WB stage
           operand_A=RWrdata_WB;  //include both load arithmetic
-          wait_signal_c=1'b0;
       end
       else if (forward_select_A==2'b10) begin //forward the data at Mem
-        wait_signal_c=1'b0;
         if(opcode_Mem==`I_TYPE_LOAD)
           operand_A=Data_mem_Mem;   // load operation
         else
           operand_A=ALU_output_Mem;   //Arithmetic operation
       end
       // Operand B forwarding
-      if(forward_select_B==2'b01) begin  //forward the data at WB stage
+      if (forward_select_B==2'b11) begin
+          operand_B=Rdata2;
+      end
+      else if(forward_select_B==2'b01) begin  //forward the data at WB stage
           operand_B=RWrdata_WB;  //include both load arithmetic
-          wait_signal_c=1'b0;
       end
       else if (forward_select_B==2'b10) begin //forward the data at Mem
-        wait_signal_c=1'b0;
         if(opcode_Mem==`I_TYPE_LOAD)
           operand_B=Data_mem_Mem;   // load operation
         else
           operand_B=ALU_output_Mem;   //Arithmetic operation
       end
-      // //error scnario
-      // if (forward_select_A==2'b00 && forward_select_B==2'b00) begin
-      //   //error
-      //   operand_A=32'b0;
-      //   operand_B=32'b0;
-      //   wait_signal_c=1'b0;
-      // end
     end
     //no conflict in this cycle, calculate branch and JALR 
     if (wait_signal_c==1'b0) begin 
+      //check prediction result
+      if (imm32[31]==1'b1) begin  // it is a jump back, predict PC jump
+        if (branch_result==1'b1) begin  //right prediction,  the PC has jumped, continue
+          predict_result=2'b11; 
+        end
+        else if (branch_result==1'b0) begin //wrong prediction, the PC doesnt jump, flush 1 stage and reload the PC 
+          predict_result=2'b01;
+        end
+      end
+      else if (imm32[31]=1'b0) begin  // no jump back, predict PC+4
+        if (branch_result==1'b1) begin  //  predict wrong, PC should jump, flush 1 stage and load PC + imm
+          predict_result=2'b10;
+        end
+        else if (branch_result==1'b0) begin //  predict right, PC not jump, continue 
+            predict_result=2'b11; 
+        end
+      end
+      else begin
+        predict_result=2'b0;
+      end
       // claculate next PC value and and PC select control signal 
       if (ALU_Control == `JALR) begin  // jump operation 
         jump_target_PC=operand_A+imm32;
+        branch_jump_flag=1'b0;
         jump_flag=1'b1;
       end
       else if (branch_op==1'b1) begin    // branch operation 
-        jump_target_PC=PC + imm32;
+        if (predict_result==2'b01) begin
+          jump_target_PC=PC+4;
+          branch_jump_flag=1'b1;
+        end
+        else if (predict_result==2'b10) begin
+          jump_target_PC=PC + imm32;
+          branch_jump_flag=1'b1;
+        end
+        else begin
+          jump_target_PC=32'b0;  //error 
+          branch_jump_flag=1'b0;
+        end
         jump_flag=1'b0;
       end
       else begin
         jump_target_PC=32'b0;
         jump_flag=1'b0;
+        branch_jump_flag=1'b0;
       end
-      branch_jump_flag = branch_op & branch_result;  // branch will be executed 
+      // assign next pc control signal
       if (jump_flag||branch_jump_flag) begin   // identify jump operation
           npc_control=1'b1;
       end
@@ -231,6 +249,7 @@ always @(*) begin
   /* not a branch or JALR, normal forwarding check, check any conflict of register */
   //start
   else begin 
+    // dont care about WB situation because path is short compared to branch path
     if (Rd_Mem!=5'b0 && Rd_Mem==Rsrc1 && mem_wEn_Mem==1'b1) begin //not a store operation and write back desitination same as required reg address
       forward_select_A_Dec=2'b01;  // forward WB data to Exec in next cycle
     end
