4839efb9b9995a4f446c5cc92da32ae69ff5c0ab
SiLin <145307129+SilinYe2001@users.noreply.github.com>
2024-02-21 19:19:32 -0600

    Add files via upload

---
+++ Benchmarks/Datahazardtest/case1,2.s
@@ -0,0 +1,8 @@
+addi x1,x0,1
+addi x2,x1,1
+addi x3,x2,1
+addi x4,x3,1
+add x5,x2,x3
+add x6,x2,x4
+add x7,x2,x5
+add x13,x6,x7
---
+++ Benchmarks/Datahazardtest/case4,5.s
@@ -0,0 +1,7 @@
+addi x3,x0,7
+addi x1,x0,3
+sw x1,0,x0
+lw x2,0,x0
+sw x2,4,x0
+lw x3,4,x0
+sw x2,1,x3
